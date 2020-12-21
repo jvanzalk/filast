af8403f20be3f3c40f9922f5bb03afa29f7fbe4c
John van Zalk <jvanzalk@SEPA.local>
2020-12-21 09:04:46 -0500

    Add setup.py file

--- setup.py
+++ setup.py
@@ -1,1 +1,16 @@
+from setuptools import setup
 
+setup(
+    name='filast',
+    url='https://github.com/jvanzalk/filast',
+    author='John van Zalk',
+    author_email='johnvanzalk@gmail.com',
+    packages=['filast'],
+    # Dependencies
+    #install_requires=['numpy'],
+    version='0.1',
+    license='',
+    description='A tool for determining email syntaxes and predicting emails',
+    # Readme
+    # long_description=open('README.txt').read(),
+)
